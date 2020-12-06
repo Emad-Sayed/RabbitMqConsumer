e8c54fc8a2d88d789820ada6dfc7b4d185de4ba2
unknown <emad_tttt522@hotmail.com>
2020-12-06 18:25:49 +0000

    v1

---
+++ RabbitMqConsumer/TicketConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using RabbitMQ.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace RabbitMqConsumer
+{
+    //Dont Forget the model name space should be the same in publisher and cosnumer
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
